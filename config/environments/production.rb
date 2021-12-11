e9f3802806da831792b46b042d36bf5466affc0e
Anibal <cubanibal@hotmail.com>
2021-12-11 14:42:52 +0100

    ultimo de verdad

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -118,6 +118,6 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   config.i18n.available_locales = :es
-  config.action_mailer.default_url_options = { host: 'curroanibal.herokuapp.com/' }
+  config.action_mailer.default_url_options = { host: 'ancient-headland-29016.herokuapp.com/' }
 
 end
